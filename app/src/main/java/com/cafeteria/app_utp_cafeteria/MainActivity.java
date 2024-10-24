83bca711dcfdebf507ce865fd2c23f5b58bc1e99
Antuane Bonilla <u20100920@utp.edu.pe>
2024-10-24 03:09:34 -0500

    barra de navegación

--- app/src/main/java/com/cafeteria/app_utp_cafeteria/MainActivity.java
+++ app/src/main/java/com/cafeteria/app_utp_cafeteria/MainActivity.java
@@ -1,24 +1,27 @@
 package com.cafeteria.app_utp_cafeteria;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Vinculando el BottomNavigationView
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        // Configurar el NavHostFragment para manejar la navegación
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_main);
+        NavController navController = navHostFragment.getNavController();
+        // Vincular el BottomNavigationView con el NavController
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
