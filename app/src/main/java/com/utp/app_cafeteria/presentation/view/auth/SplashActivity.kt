cfbc58bf8f8113eff2aafe46f7c8cb9b43bdcc09
Antuane Bonilla <u20100920@utp.edu.pe>
2024-11-12 04:22:20 -0500

    creación de splash screen

---
+++ app/src/main/java/com/utp/app_cafeteria/presentation/view/auth/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.utp.app_cafeteria.presentation.view.auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.utp.app_cafeteria.MainActivity
+import com.utp.app_cafeteria.R
+import kotlinx.coroutines.*
+
+class SplashActivity : AppCompatActivity() {
+
+    // Define un Job para la coroutine
+    private val splashJob = Job()
+    private val splashScope = CoroutineScope(Dispatchers.Main + splashJob)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash) // Cambia a tu XML splash
+
+        // Ejecuta el retraso de 3 segundos
+        splashScope.launch {
+            delay(5000) // Espera 3 segundos
+            // Después del retraso, inicia MainActivity
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish() // Finaliza el SplashActivity para que no regrese al presionar atrás
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        splashJob.cancel() // Cancela el job cuando la actividad se destruye
+    }
+}
