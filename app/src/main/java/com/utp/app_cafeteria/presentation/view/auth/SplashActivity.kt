7aa53cb31b63cd059c0a18e99aac6fb1b3ed39eb
Antuane Bonilla <u20100920@utp.edu.pe>
2024-11-19 16:47:15 -0500

    modificación splash screen

--- app/src/main/java/com/utp/app_cafeteria/presentation/view/auth/SplashActivity.kt
+++ app/src/main/java/com/utp/app_cafeteria/presentation/view/auth/SplashActivity.kt
@@ -2,34 +2,65 @@ package com.utp.app_cafeteria.presentation.view.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import com.utp.app_cafeteria.MainActivity
 import com.utp.app_cafeteria.R
 import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
 
-    // Define un Job para la coroutine
     private val splashJob = Job()
     private val splashScope = CoroutineScope(Dispatchers.Main + splashJob)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash) // Cambia a tu XML splash
+        setContentView(R.layout.activity_splash)
 
-        // Ejecuta el retraso de 3 segundos
+
+        val topBanner: ImageView = findViewById(R.id.imageView2)
+        val bottomBanner: ImageView = findViewById(R.id.imageView5)
+        val logo: ImageView = findViewById(R.id.imageView)
+        val lottieAnimation: LottieAnimationView = findViewById(R.id.animationView)
+        val sloganText: TextView = findViewById(R.id.textView4)
+
+
+        val bannerInLeft = AnimationUtils.loadAnimation(this, R.anim.banner_in_left)
+        val bannerInRight = AnimationUtils.loadAnimation(this, R.anim.banner_in_right)
+        val zoomLogo = AnimationUtils.loadAnimation(this, R.anim.zoom_logo)
+        val fadeInLottie = AnimationUtils.loadAnimation(this, R.anim.fade_in_lottie)
+
+
+        topBanner.startAnimation(bannerInLeft)
+        bottomBanner.startAnimation(bannerInRight)
+        logo.startAnimation(zoomLogo)
+        lottieAnimation.startAnimation(fadeInLottie)
+
+        applyTypewriterEffect(sloganText, getString(R.string.eslogan))
+
         splashScope.launch {
-            delay(5000) // Espera 3 segundos
-            // Después del retraso, inicia MainActivity
+            delay(5000)
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             overridePendingTransition(R.anim.slide_fade_in, R.anim.fade_out)
+            finish()
+        }
+    }
 
-            finish() // Finaliza el SplashActivity para que no regrese al presionar atrás
+    private fun applyTypewriterEffect(textView: TextView, text: String) {
+        textView.text = ""
+        CoroutineScope(Dispatchers.Main).launch {
+            for (char in text) {
+                textView.append(char.toString())
+                delay(20)
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        splashJob.cancel() // Cancela el job cuando la actividad se destruye
+        splashJob.cancel()
     }
 }
